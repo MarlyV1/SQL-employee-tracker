9fe7530b702d8a24f63565791a4e7f562394d9c1
Marly Valcin <marlyvalcin@gmail.com>
2024-06-02 22:49:47 -0400

    created addDepartment function

--- index.js
+++ index.js
@@ -15,7 +15,39 @@ function listOptions() {
     return inquirer.prompt(listQuestion)
     .then((data) => {
     data = data.listOptions;
+    // console.log(data);
+    
+    if (data === "View All Employees") {
+
+    }else if(data === "Add Employee") {
+
+    }else if (data === "Update Employee Role") {
+
+    }else if (data === "Add Role") {
+
+    }else if(data === "View All Departments") {
+
+    }else if (data === "Add Department"){
+
+    }
+    return;
+    // return listOptions();
+    })
+};
+
+function addDepartment() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "department",
+            message: "What is the name of the department?"
+        }
+    ])
+    .then((data) => {
+        
+        console.log(`Added ${data.addDepartment} to the database`)
     })
 }
+
 listOptions();
 
